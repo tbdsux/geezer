0f8682322d2f9daad3c7bf66459f3d71c01b6a97
TheBoringDude <iamcoderx@gmail.com>
2020-10-03 13:52:12 +0800

    Change Keyboard Shortut binding keys.

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,13 @@ import '../css/tailwind.css'
 const Mousetrap = require('mousetrap')
 
 class Geezer extends App {
+  // use the common keyboard shortcuts for easier page refresh
   componentDidMount() {
-    Mousetrap.bind("enter", function() {
+    Mousetrap.bind("space", function() {
         window.location.reload()
+    })
+    Mousetrap.bind("right", function() {
+      window.location.reload()
     })
   }
   render() {
