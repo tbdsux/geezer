9c4190a4f952a7a07fdf4af1165b684c04bc84c9
TheBoringDude <iamcoderx@gmail.com>
2020-10-24 10:28:35 +0800

    Change Meme API Source and add Meme title

--- components/MemeContainer.js
+++ components/MemeContainer.js
@@ -1,16 +1,25 @@
- export default function MemeContainer({meme}) {
-    if (meme.endsWith("mp4")) {
-        return (
-            <div className="my-4">
-                <video autoPlay loop className="h-100 w-auto">
-                    <source src={`${meme}`} type="video/mp4"></source>
-                </video>
-            </div>
-        )
-    }
+export default function MemeContainer({ meme }) {
+  // for video gifs
+  if (meme.src.endsWith('mp4')) {
     return (
-        <div className="my-4">
-            <img src={`${meme}`} alt="" className="h-100 w-auto object-cover" />
-        </div>
+      <div className="my-4">
+        <h3 className="text-center py-1 text-white font-light tracking-wide">
+          {meme.title}
+        </h3>
+        <video autoPlay loop className="h-100 w-auto">
+          <source src={`${meme.src}`} type="video/mp4"></source>
+        </video>
+      </div>
     )
+  }
+
+  // just for imgs
+  return (
+    <div className="my-4">
+      <h3 className="text-center py-1 text-white font-light tracking-wide">
+        {meme.title}
+      </h3>
+      <img src={`${meme.src}`} alt="" className="h-100 w-auto object-cover" />
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,44 @@
 import Head from 'next/head'
-import Layout, {SiteName} from '../components/Layout'
+import Layout, { SiteName } from '../components/Layout'
 import MemeContainer from '../components/MemeContainer'
 
 const today = new Date()
 
 function reloadWebsite() {
-    window.location.reload()
+  window.location.reload()
 }
 
-function Home({data}) {
-    return (
-        <Layout>
-            <Head>
-                <title>{SiteName} - Random Memer</title>
-            </Head>
+function Home({ data }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{SiteName} - Random Memer</title>
+      </Head>
 
-            {/* posts container */}
-            <div className="flex items-center justify-center h-full py-16 w-full mx-auto">
-                <div className="text-center">
-                    <h1 className="text-lighter font-black text-4xl tracking-wide uppercase">Geezer</h1>
-                    <MemeContainer meme={data.meme} />
-                    <button onClick={reloadWebsite} className="px-8 mt-4 py-3 bg-mid-blue text-white text-sm font-light tracking-wide">next</button>
-                </div>
-            </div>
-        </Layout>
-    )
+      {/* posts container */}
+      <div className="flex items-center justify-center h-full py-16 w-full mx-auto">
+        <div className="text-center">
+          <h1 className="text-lighter font-black text-4xl tracking-wide uppercase">
+            Geezer
+          </h1>
+          <MemeContainer meme={data.meme} />
+          <button
+            onClick={reloadWebsite}
+            className="px-8 mt-4 py-3 bg-mid-blue text-white text-sm font-light tracking-wide"
+          >
+            next
+          </button>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export async function getServerSideProps() {
-    const res = await fetch(`http://gagger.herokuapp.com/`)
-    const data = await res.json()
+  const res = await fetch(`https://gagger-serverless.vercel.app/`)
+  const data = await res.json()
 
-    return {props: {data}}
+  return { props: { data } }
 }
 
 export default Home
